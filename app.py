fbe45df1c9ee6a6ab129c37bf828dff8a4756261
panwan1040 <6206021621040@fitm.kmutnb.ac.th>
2022-10-22 17:52:05 +0700

    พัง1

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import json
 from flask import (
     Flask, render_template, request, Markup
 )
@@ -10,23 +11,51 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/submit', methods=['GET', 'POST'])
-def encoder():
+def encryptRailFence(text, key):
+
+    rail = [['\n' for i in range(len(text))]
+            for j in range(key)]
+
+    # to find the direction
+    dir_down = False
+    row, col = 0, 0
+
+    for i in range(len(text)):
+
+        if (row == 0) or (row == key - 1):
+            dir_down = not dir_down
+
+        rail[row][col] = text[i]
+        col += 1
+
+        if dir_down:
+            row += 1
+        else:
+            row -= 1
+
+    result = []
+    for i in range(key):
+        for j in range(len(text)):
+            if rail[i][j] != '\n':
+                result.append(rail[i][j])
+    return("" . join(result))
+
+
+@app.route('/submit/<string:mytext>/<string:mykey>', methods=['GET', 'POST'])
+def submit(mytext, mykey):
     if request.method == "POST":
-        text = request.form['text'].strip()
-        keys = request.form['keys']
+        text = json.loads(mytext).strip()
+        keys = json.loads(mykey)
+        print(text, keys, "<----------------------")
 
-        def fence_pattern(rails, message_size):
-            r = 2 * (rails - 1)
-            return sorted(((z % r) if (z % r) < rails else r - (z % r), z) for z in range(message_size))
-
-        def encode(msg, rails):
-            return ''.join(msg[i] for _, i in fence_pattern(rails, len(msg)))
-        data = encode(text, int(keys))
+        # text = request.form['text'].strip()
+        # text = "ggggggggggggg"
+        # keys = request.form['keys']
+        data = encryptRailFence(text, int(keys))
+        print(data)
         return(data)
     return render_template('index.html')
 
 
-# esan
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ static/javascript/script.js
@@ -0,0 +1,199 @@
+$(".type").keyup(function() {
+    if (!$(this).text().trim().length) {
+        $(this).prepend('<span class="caret"></span>');
+    }
+});
+
+// $("#output").hide();
+// $(document).ready(function () {
+//   $("#form").on("submit", function (e) {
+//     $.ajax({
+//       data: {
+//         text: $("#textarea").val(),
+//         keys: $("#keys").val(),
+//       },
+//       type: "POST",
+//       url: "/submit",
+//       beforeSend: function () {
+//         $("#loader").show();
+//       },
+//     }).done(function (data) {
+//       $("#loader").hide();
+//       var tt = $("#textarea").val();
+//       if (tt != "") $("#output").html(data).show();
+//       // else $("#output").hide();
+//     });
+//     e.preventDefault();
+//   });
+// });
+
+
+// ------------------- get text
+
+function getmytext_andpasstopy(){
+  let mytext = document.getElementById('mytext').textContent;
+  let mykey = document.getElementById('keys').value;
+  console.log(mytext);
+  const req =  new XMLHttpRequest()
+  req.open('POST',`/submit/${JSON.stringify(mytext)}/${JSON.stringify(mykey)}`)
+  req.send()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// --------------------
+
+const colors = ['orange','orangered','darkorange', 'gold', 'tomato', 'goldenrod','yellow'],
+      fire_spread = 15 // best range 10-20
+
+function addParticle() {
+  var fs = document.createElement('div'),
+      skew = Math.random() < .5 ? Math.random()*fire_spread : -Math.random()*fire_spread
+  fs.className = 'fire_shaft'
+  fs.style.height = Math.random()*50 + 25 + 'vh'
+  fs.style.transform = 'skew('+skew+'deg)'
+  fs.style.left = Math.random()*100 + '%'
+  var p = document.createElement('div')
+  p.className = 'particle'
+  p.style.background = colors[Math.floor(Math.random()*colors.length)]
+  p.onanimationend = function() { this.remove() }  
+  document.body.appendChild(fs).appendChild(p)
+}
+
+setInterval(addParticle,1000/60)
+
+
+
+function handleKeyUp(event) {
+  const BASE_FONT_SIZE = 10;
+  const MIN_FONT_SIZE = 1.5;
+
+  const input = event.target;
+
+  const inputLength = input.innerText.length;
+  const inputFontSize = input.style.fontSize;
+
+  let newFontSize = BASE_FONT_SIZE;
+
+  if (inputLength > BASE_FONT_SIZE) {
+    newFontSize = BASE_FONT_SIZE - Math.ceil(inputLength / BASE_FONT_SIZE);
+  }
+
+  input.style.fontSize =
+    newFontSize < MIN_FONT_SIZE ? `${MIN_FONT_SIZE}rem` : `${newFontSize}rem`;
+}
+
+function handleBlur(event) {
+  const input = event.target;
+
+  if (!input.innerText.trim().length) {
+    input.innerHTML = "";
+  }
+}
+
+const input = document.querySelector(".input");
+
+input.addEventListener("keyup", handleKeyUp);
+input.addEventListener("blur", handleBlur);
+
+
+// 
+
+
+
+const elts = {
+  text1: document.getElementById("text1"),
+  text2: document.getElementById("text2")
+};
+
+const texts = [
+  "Rail",
+  "Fence",
+  "Encoder",
+  "Rail Fence Encoder",
+  ":)"
+];
+
+const morphTime = 1;
+const cooldownTime = 0.25;
+
+let textIndex = texts.length - 1;
+let time = new Date();
+let morph = 0;
+let cooldown = cooldownTime;
+
+elts.text1.textContent = texts[textIndex % texts.length];
+elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+
+function doMorph() {
+  morph -= cooldown;
+  cooldown = 0;
+
+  let fraction = morph / morphTime;
+
+  if (fraction > 1) {
+      cooldown = cooldownTime;
+      fraction = 1;
+  }
+
+  setMorph(fraction);
+}
+
+function setMorph(fraction) {
+  elts.text2.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+  elts.text2.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+
+  fraction = 1 - fraction;
+  elts.text1.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+  elts.text1.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+
+  elts.text1.textContent = texts[textIndex % texts.length];
+  elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+}
+
+function doCooldown() {
+  morph = 0;
+
+  elts.text2.style.filter = "";
+  elts.text2.style.opacity = "100%";
+
+  elts.text1.style.filter = "";
+  elts.text1.style.opacity = "0%";
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  let newTime = new Date();
+  let shouldIncrementIndex = cooldown > 0;
+  let dt = (newTime - time) / 1000;
+  time = newTime;
+
+  cooldown -= dt;
+
+  if (cooldown <= 0) {
+      if (shouldIncrementIndex) {
+          textIndex++;
+      }
+
+      doMorph();
+  } else {
+      doCooldown();
+  }
+}
+
+animate();
+
+
+
+
